a3c3e9dbc212bacf4c0265df1bd723ddc2eba865
Antonio Raian <raymendesjr2013@gmail.com>
2021-06-03 15:31:30 -0300

    feat: Add a validate route and change the away to do requests on deal

--- controllers/DealsPipeController.js
+++ controllers/DealsPipeController.js
@@ -1,23 +1,39 @@
 const axios = require("axios");
 const faker = require("faker");
+const { validateReq } = require("../utils/validateRequests");
 const OrdersBlingController = require("./OrdersBlingController");
 
 // PipeDrive commons
-const apiKey = "236ffe4fee7849d756c5b7c6416c8be007a9ee8e";
-const companyName = "teste221";
+// const apiKey = "236ffe4fee7849d756c5b7c6416c8be007a9ee8e";
+// const companyName = "teste221";
 
 // Methods
 // Used to create fakers data on pipedrive
 exports.create = async (req, res) => {
+  // Validate datas
+  validateReq(req, res);
+
+  // Get datas from request
+  const { body, query } = req;
+
+  // PipeDrive commons
+  const apiKey = query.pipi_key;
+  const companyName = query.company_name;
+
+  // Choose what go send to create a Deal
+  const bodyToSend = {
+    ...body,
+    title: body?.title || faker.name.title(),
+    value: body?.value || faker.datatype.number(2000),
+    // status: 'won',
+    status: body?.status || faker.helpers.randomize(["open", "won", "lost"]),
+  };
+
+  // Request to create a Deal on Pipe Drive API
   await axios
     .post(
       `https://${companyName}.pipedrive.com/api/v1/deals?api_token=${apiKey}`,
-      {
-        title: faker.name.title(),
-        value: faker.datatype.number(2000),
-        // status: 'won',
-        status: faker.helpers.randomize(["open", "won", "lost"]),
-      }
+      bodyToSend
     )
     .then((resp) => {
       res.status(200).send(resp.data); // Return the data from Pipe Drive's Api
@@ -29,8 +45,15 @@ exports.create = async (req, res) => {
 
 // Used to get the Pipi Drive Deals and send to create on bling anda loval Database
 exports.list = async (req, res) => {
+  // Validate datas
+  validateReq(req, res);
+
   // Get all query possiblities from url request
   const params = req.query;
+
+  // PipeDrive commons
+  const apiKey = params.pipi_key;
+  const companyName = params.company_name;
 
   // Format the actual Date in patterns to Pipi Drive's Api
   const now = new Date();
@@ -41,8 +64,9 @@ exports.list = async (req, res) => {
   // Select between the default data or query data
   const startDate = params.start_date || formatedDate;
   const interval = params.interval || "day";
-  const amount = params.amout || 1;
+  const amount = params.amount || 1;
   const fieldKey = params.field_key || "won_time";
+  const createOrder = params.create_orders || false;
 
   // Request on Pipe Drive's Api
   await axios
@@ -52,9 +76,14 @@ exports.list = async (req, res) => {
     )
     .then(async (resp) => {
       // On success, Try Create a new Order on Bling with response and show error from create or the data from Pipe Drive
-      res
-        .status(200)
-        .send((await OrdersBlingController.create(resp.data)) || resp.data);
+      const created = createOrder
+        ? await OrdersBlingController.create({
+            ...resp.data,
+            bling_api: params.bling_key,
+          })
+        : undefined;
+
+      res.status(200).send(created || resp.data);
     })
     .catch((error) => {
       res.send(error); // Return the error
--- routes/dealsRoutes.js
+++ routes/dealsRoutes.js
@@ -1,12 +1,47 @@
 const express = require("express");
+const { query } = require("express-validator");
 const { list, create } = require("../controllers/DealsPipeController");
 
 const router = express.Router();
 
 // All the routes to access the functions of PipiBlingApi
 // The route  GET '../deals/ store the new orders on bling from the pipe drive won on day
-router.get("/", list);
+router.get(
+  "/",
+  // Set validator to attributes required
+  query("pipe_key")
+    .exists()
+    .withMessage("pipe_key required")
+    .notEmpty()
+    .withMessage("pipe_key not be empty"),
+  query("company_name")
+    .exists()
+    .withMessage("company_name required")
+    .notEmpty()
+    .withMessage("company_name not be empty"),
+  list
+);
 // The route POST '../deasl/ create fakes datas to pipe drive
-router.post("/", create);
+router.post(
+  "/",
+  query("pipe_key")
+    .exists()
+    .withMessage("pipe_key required")
+    .notEmpty()
+    .withMessage("pipe_key not be empty"),
+  query("company_name")
+    .exists()
+    .withMessage("company_name required")
+    .notEmpty()
+    .withMessage("company_name not be empty"),
+  query("bling_key").custom((value, { req }) => {
+    if (req.query.create_orders && !value) {
+      throw new Error(
+        'For create Order enter Bling Api Key on "bling_key" attribute'
+      );
+    }
+  }),
+  create
+);
 
 module.exports = router;
